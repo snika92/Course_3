cf05ee1eaf749d266beee36086b4810fa64109c5
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-06 00:32:12 +0300

    add format_date() to operation.py

--- src/operation.py
+++ src/operation.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Operation:
 
     def __init__(self, id, description, summa, currency, date, to_card_name,
@@ -15,3 +18,11 @@ class Operation:
     def __repr__(self):
         return f'Operation({self.id}, {self.description}, {self.summa}, {self.currency}, ' \
                f'{self.date}, {self.to_card_name}, {self.to_account}, {self.from_card_name}, {self.from_account})'
+
+    def format_date(self):
+        """
+        Приводит дату к формату ДД.ММ.ГГГГ (пример: 14.10.2018) и возвращает её
+        """
+        self.date = datetime.strptime(self.date[0], '%Y-%m-%d')
+        self.date = self.date.strftime('%d.%m.%Y')
+        return self.date
