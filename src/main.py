b9bdf5dd10bd8e8bd02aafc6a223ae7f2f2560fc
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-06 00:30:11 +0300

    get data from json file, make list of executed operations, sort by date and get latest 5 operations

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from utils.utils import load_operations
+from utils.utils import make_list_executed_operations
+
+
+if __name__ == '__main__':
+
+    # Получаем данные и файла и формируем список выполненных операций
+    filename = "operations.json"
+    data = load_operations(filename)
+    list_of_executed_operations = make_list_executed_operations(data)
+
+    # Сортируем список по дате, от последней операции к первой и получаем последние 5 операций
+    sorted_list_by_date = sorted(list_of_executed_operations, key=lambda x: x.date, reverse=True)
+    latest_five = sorted_list_by_date[:5]
