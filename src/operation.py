a498f680ad003c995cec261e2695a1b0722f4250
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-08 02:11:22 +0300

    fix mask_from_account() and add return_from_card_name()

--- src/operation.py
+++ src/operation.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Operation:
 
     def __init__(self, id, description, summa, currency, date, to_card_name,
-                 to_account, from_card_name="None", from_account="None"):
+                 to_account, from_card_name=None, from_account=None):
         self.id = id
         self.description = description
         self.summa = summa
@@ -27,14 +27,6 @@ class Operation:
         self.date = self.date.strftime('%d.%m.%Y')
         return self.date
 
-    def mask_from_account(self):
-        """
-        Маскирует номер карты/счёта отправителя, возвращает в формате  XXXX XX** **** XXXX
-        (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом).
-        """
-        self.from_account = str(self.from_account)
-        return f"{self.from_account[:4]} {self.from_account[4:6]}** **** {self.from_account[-4:]}"
-
     def mask_to_account(self):
         """
         Маскирует номер карты/счёта получателя, возвращает в формате  **XXXX
@@ -42,3 +34,23 @@ class Operation:
         """
         self.to_account = str(self.to_account)
         return f"**{self.to_account[-4:]}"
+
+    def mask_from_account(self):
+        """
+        Маскирует номер карты/счёта отправителя, возвращает в формате  XXXX XX** **** XXXX
+        (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом).
+        """
+        if self.from_account is None:
+            return "Не указано"
+        else:
+            self.from_account = str(self.from_account)
+            return f"{self.from_account[:4]} {self.from_account[4:6]}** **** {self.from_account[-4:]}"
+
+    def return_from_card_name(self):
+        """
+        Возвращает название карты отправителя, либо фразу "Не указано", если self.card.name = None
+        """
+        if self.from_card_name is None:
+            return "Не указано"
+        else:
+            return self.from_card_name
