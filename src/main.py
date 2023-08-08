3d7287cf664d4002ac774691c9bcf7e3808d7952
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-08 22:37:30 +0300

    fix output

--- src/main.py
+++ src/main.py
@@ -15,6 +15,4 @@ if __name__ == '__main__':
 
     # Выводим последние 5 операций в требуемом формате
     for item in latest_five:
-        print(f"""{item.format_date()} {item.description}
-{item.return_from_card_name()} {item.mask_from_account()} -> {item.to_card_name} {item.mask_to_account()}
-{item.summa} {item.currency}\n""")
+        print(item.__repr__())
